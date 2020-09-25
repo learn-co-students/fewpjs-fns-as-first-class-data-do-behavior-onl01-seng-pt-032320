bc07a0a936b41c192c593a59dc8049127f260596
nabilhayet <nabil4457@yahoo.com>
2020-09-24 20:59:27 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  const parsed = parseInt(time, 10);
+  if (parsed < 12.00){
+    return "Good Morning"
+  }
+    elsif (parsed > 12.00 && parsed < 17.00){
+      return "Good Afternoon"
+    }
+    elsif(parsed > 17.00){
+      return "Good Evening"
+    }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(str){
+
+}
