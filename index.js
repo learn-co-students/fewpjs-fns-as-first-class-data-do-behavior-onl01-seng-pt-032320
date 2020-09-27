6d123259af9a58900d3a77aed3b61e622c47112d
Shamel Lakin <christlife4u@gmail.com>
2020-09-27 11:54:43 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  let on_time= time.split(":"); //split to remove colon returns two strings
+  let great_time= on_time[0].concat(on_time[1]) //concat joins the strings
+  let result_time= parseInt(great_time, 10) //parseInt converts strings into numbers
+
+  if (result_time < 1200){
+    return "Good Morning";
+  }
+  else if (result_time > 1700){
+    return "Good Evening";
+  }
+  else {
+    return "Good Afternoon";
+  }
+  
+}
+  
+// function display(update){ 
+//   let update = "Test"
+//   document.querySelector('#input');
+   
+//   }
