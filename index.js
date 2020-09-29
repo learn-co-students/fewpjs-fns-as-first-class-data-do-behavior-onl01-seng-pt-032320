072b74927bebd7337cca8e53dcd149a5e798c0dd
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-29 03:07:47 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let parsedTime = parseInt(time.split(":")[0]);
+  if (parsedTime<12) {
+    return "Good Morning";
+  } else if (parsedTime>=12 && parsedTime<17) {
+    return "Good Afternoon";
+  } else if (parsedTime>=17 && parsedTime<24) {
+    return "Good Evening";
+  } else {
+    return "Error!";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(mess) {
+  document.getElementById("greeting").innerText = mess;
+}
