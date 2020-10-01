b2ebd88712b9acc91bdc97d562e49ac7ef978012
Shamel Lakin <christlife4u@gmail.com>
2020-10-01 16:38:51 -0400

    Done.

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function greet(time) {
   let great_time= on_time[0].concat(on_time[1]) //concat joins the strings
   let result_time= parseInt(great_time, 10) //parseInt converts strings into numbers
 
+
   if (result_time < 1200){
     return "Good Morning";
   }
@@ -31,7 +32,8 @@ function greet(time) {
   
 //update the DOM
   function displayMessage(greet){
-    let element = document.querySelector("input#time");
-    element.innerHTML = 'TEST'
+    let element = document.querySelector("h1");
+    element.innerText = greet
+    return greet
   } 
   
