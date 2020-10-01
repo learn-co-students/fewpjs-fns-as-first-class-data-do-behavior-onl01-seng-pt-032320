e34cd82745ae90d503e48d0527e582b14d2a15e9
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-09-30 23:08:48 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+  let hour = parseInt(time.split(":")[0])
+  if (hour < 12) return "Good Morning";
+  if (hour >= 17) return "Good Evening";
+  return "Good Afternoon";
+} 
+
+function displayMessage(greeting){
+  document.getElementById('greeting').innerText = greeting;
+
+}
